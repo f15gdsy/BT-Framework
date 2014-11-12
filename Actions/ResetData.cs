84c37149a932c3fffb5cd56620b9abbef2c3685b
TheGameFather <f15gdsy@gmail.com>
2014-11-12 20:18:56 +0800

    - Refactor ResetData to remove Jargon - Added data id support

--- Actions/ResetData.cs
+++ Actions/ResetData.cs
@@ -2,17 +2,28 @@
 using System.Collections;
 using BT;
 
+/// <summary>
+/// Reset the data in database to 
+/// 	1. default data, passed in through constructor, or
+/// 	2. another data in database, whose name is passed in through constructor.
+/// </summary>
 public class ResetData<T> : BTAction {
 	private string _dataToReset;
+	private int _dataIdToReset;
+
 	private T _defaultData;
+
 	private string _targetData;
+	private int _targetDataId;
+
+	// Indicate if the data should be reset when this node is cleared 
+	// Clear is called (when it ends, or when its parent ends).
 	private bool _shouldClear;
 	
 	
 	public ResetData (string dataToReset, T defaultData, bool shouldClear = false) : base (null) {
 		_dataToReset = dataToReset;
 		_defaultData = defaultData;
-		_targetData = Jargon.Invalid;
 		_shouldClear = shouldClear;
 	}
 
@@ -21,13 +32,20 @@ public class ResetData<T> : BTAction {
 		_targetData = targetData;
 		_shouldClear = shouldClear;
 	}
+
+	public override void Activate (Database database) {
+		base.Activate (database);
+
+		_dataIdToReset = database.GetDataId(_dataToReset);
+		_targetDataId = _targetData == null ? -1 : database.GetDataId(_targetData);
+	}
 	
 	protected override BTResult Execute () {
-		if (_targetData.Equals(Jargon.Invalid)) {
-			database.SetData<T>(_dataToReset, _defaultData);
+		if (_targetData == null) {
+			database.SetData<T>(_dataIdToReset, _defaultData);
 		}
 		else {
-			T data = database.GetData<T>(_targetData);
+			T data = database.GetData<T>(_targetDataId);
 			database.SetData<T>(_dataToReset, data);
 		}
 
